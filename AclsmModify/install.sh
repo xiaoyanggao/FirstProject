f3dc2ff28f2d1a286b5d05b41fab5f5572f37014
xiaoyanggao <2951351572@qq.com>
2018-03-23 11:32:39 +0800

    modify

--- AclsmModify/install.sh
+++ AclsmModify/install.sh
@@ -2,6 +2,3 @@
 echo '初始化数据库表和登陆账户'
 python manage.py makemigrations
 python manage.py migrate
-echo "from Integrated.models import UserProfile as User; User.objects.create_superuser(username='yangxu', email='yangxu1@kingsoft.com', password='a8556432')" | python manage.py shell
-
-echo "账号、密码：yangxu1@kingsoft.com/a8556432"
